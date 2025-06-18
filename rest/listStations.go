47b2e2841720aa407f9f1349d51d1a603d58e771
Tamara <tomatenstrauch1234567890@gmail.com>
2025-06-18 20:44:55 +0200

    a little openapi business logic

---
+++ rest/listStations.go
@@ -0,0 +1,37 @@
+// Edit this file, as it is a specific handler function for your service
+package rest
+
+import (
+	"build/core/log"
+	"build/core/tracing"
+	"build/usecases"
+
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+// List all weather stations
+func ListStations(c echo.Context) error {
+	// trace span
+	ctx := c.Request().Context()
+	ctx, span := tracing.Tracer.Start(ctx, "logMessage")
+	defer span.End()
+
+	traceId := span.SpanContext().TraceID().String()
+	spanId := span.SpanContext().SpanID().String()
+	log.Info().Str("traceId", traceId).Str("spanId", spanId).Str("path", "/").Msg("ListStations")
+
+	// session, err := getSession(c)
+	// if err != nil {
+	// 	log.Error().Err(err).Msg("ListStations failed")
+	// 	return c.NoContent(http.StatusInternalServerError)
+	// }
+
+	// implement your functionality best using a function from a separate file, e.g. usecases/ListStationsDo.go
+	stations := usecases.ListStations()
+	return c.JSON(http.StatusOK, stations)
+
+	// 200 => A list of weather stations
+	//return c.String(http.StatusNotImplemented, "Temporary handler stub.")
+}
